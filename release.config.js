99d9755f42836f9e1a335d5d902b2032085dec9a
P1989singh <prmkmrsingh@yahoo.in>
2023-11-13 19:31:49 +0530

    fix :testing4

--- release.config.js
+++ release.config.js
@@ -1,8 +1,10 @@
-export const branches = ['main'];
-export const plugins = [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    '@semantic-release/github',
-];
+module.exports = {
+    branches: ['main'],
+    plugins: [
+      '@semantic-release/commit-analyzer',
+      '@semantic-release/release-notes-generator',
+      '@semantic-release/changelog',
+      '@semantic-release/npm',
+      '@semantic-release/github',
+    ],
+  };
