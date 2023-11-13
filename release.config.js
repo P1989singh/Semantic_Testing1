34c2ac124a7be2589f9382c78a2ba457c110f4dd
P1989singh <prmkmrsingh@yahoo.in>
2023-11-13 19:52:51 +0530

    fix:config issue

--- release.config.js
+++ release.config.js
@@ -1,10 +1,13 @@
 module.exports = {
     branches: ['main'],
     plugins: [
-      '@semantic-release/commit-analyzer',
-      '@semantic-release/release-notes-generator',
-      '@semantic-release/changelog',
-      '@semantic-release/npm',
-      '@semantic-release/github',
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
+      "@semantic-release/changelog",
+      ["@semantic-release/git",{
+        "assets": ["src/db/*.txt","src/db/*.js"],
+        "message": "chore(release): ${nextRelease.version} [skip.ci]\n\n${nextRelease.notes}"
+      }],
+      "@semantic-release/github"
     ],
   };
