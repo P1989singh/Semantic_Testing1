5c6ad5c7977fac3a20bd5a903b48731232b04ef8
P1989singh <prmkmrsingh@yahoo.in>
2023-11-13 20:04:07 +0530

    feat:testing5

--- release.config.js
+++ release.config.js
@@ -1,13 +1,14 @@
-module.exports = {
+const config = {
     branches: ['main'],
     plugins: [
       "@semantic-release/commit-analyzer",
       "@semantic-release/release-notes-generator",
       "@semantic-release/changelog",
       ["@semantic-release/git",{
-        "assets": ["src/db/*.txt","src/db/*.js"],
+        "assets": ["dist/*.js"],
         "message": "chore(release): ${nextRelease.version} [skip.ci]\n\n${nextRelease.notes}"
       }],
       "@semantic-release/github"
     ],
   };
+  module.export=config;
